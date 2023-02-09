5709f9521bad10d9154b683e89f23a791733ccd2
ryan <ryan.lq@hotmail.com>
2023-02-09 18:42:46 +0800

    update

--- userscripts/cpolar_auto_jump.js
+++ userscripts/cpolar_auto_jump.js
@@ -11,54 +11,56 @@
 
 (function() {
     'use strict';
+    window.onload = function(){
 
-    let navs = {
-        "Alist":[],
-        "apache":[],
-        "cpolarpanel":[],
-        "ssh":'',
-    }
-
-    const table = document.querySelector(".table-sm")
-    const trs = table.querySelector('tbody').querySelectorAll("tr")
-    let names = [];
-    trs.forEach(tr=>{
-        const name = tr.querySelector('td').innerText
-        const url = tr.querySelector('a').href
-        if(name == "ssh") {
-            const params = url.replace("tcp://",'').split(":")
-            navs.ssh = params[0]+ " -p "+ params[1]
-        } else {
-            url.includes("http://")?navs[name].http = url :navs[name].https = url
+        let navs = {
+            "Alist":[],
+            "apache":[],
+            "cpolarpanel":[],
+            "ssh":'',
         }
-    })
 
-    const actionid = window.location.href.replace('https://dashboard.cpolar.com/status?','')
+        const table = document.querySelector(".table-sm")
+        const trs = table.querySelector('tbody').querySelectorAll("tr")
+        let names = [];
+        trs.forEach(tr=>{
+            const name = tr.querySelector('td').innerText
+            const url = tr.querySelector('a').href
+            if(name == "ssh") {
+                const params = url.replace("tcp://",'').split(":")
+                navs.ssh = params[0]+ " -p "+ params[1]
+            } else {
+                url.includes("http://")?navs[name].http = url :navs[name].https = url
+            }
+        })
 
-    window.location.href = names[actionid].url
+        const actionid = window.location.href.replace('https://dashboard.cpolar.com/status?','')
 
-    switch(actionid){
-        case "1":
-            window.location.href = navs.Alist.http
-            break;
-        case "2":
-            window.location.href = navs.Alist.https
-            break;
-        case "3":
-            window.location.href = navs.apache.http
-            break;
-        case "4":
-            window.location.href = navs.apache.https
-            break;
-        case "5":
-            window.location.href = navs.cpolarpanel.http
-            break;
-        case "6":
-            window.location.href = navs.cpolarpanel.https
-            break;
-        case "7":
-            document.querySelector("#nav").append(navs.ssh)
-            break;
+        window.location.href = names[actionid].url
+
+        switch(actionid){
+            case "1":
+                window.location.href = navs.Alist.http
+                break;
+            case "2":
+                window.location.href = navs.Alist.https
+                break;
+            case "3":
+                window.location.href = navs.apache.http
+                break;
+            case "4":
+                window.location.href = navs.apache.https
+                break;
+            case "5":
+                window.location.href = navs.cpolarpanel.http
+                break;
+            case "6":
+                window.location.href = navs.cpolarpanel.https
+                break;
+            case "7":
+                document.querySelector("#nav").append(navs.ssh)
+                break;
+        }
     }
 
 })();
