a362ae1b7648c5df5caeb5b6a45acbac661b0f85
ryan <ryan.lq@hotmail.com>
2023-02-17 16:08:37 +0800

    Add files via upload

---
+++ userscripts/link_jump_zhihu_com.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        知乎直接跳转
+// @namespace   Violentmonkey Scripts
+// @match       *://*.zhihu.com/*
+// @grant       none
+// @version     1.0
+// @author      xdaoo
+// @description 2023/2/17 16:02:47
+// ==/UserScript==
+
+
+(function(){
+    window.onload = function(){
+      let links = document.querySelectorAll("a[href]")
+      links.forEach(link=>{
+          if(link['href'] && link['href'].match(/https:\/\/link.zhihu.com\/\?target=/)){
+              link.href = decodeURIComponent(link.href.replace("https://link.zhihu.com/?target=",""))
+          }
+      })
+    }
+  })()
