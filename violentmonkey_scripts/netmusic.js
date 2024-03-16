5b0b5917655bffb06b1ae11c6683fabaa6c44534
ryan <ryan.lq@hotmail.com>
2024-03-16 20:14:50 +0800

    update

--- violentmonkey_scripts/netmusic.js
+++ violentmonkey_scripts/netmusic.js
@@ -4,16 +4,16 @@
 // @match       https://music.163.com/*
 // @grant       GM_addStyle
 // @grant       unsafeWindow
-// @version     1.6
+// @version     1.7
 // @author      xdaoo
 // @description 2024/3/2 19:36:19
 // ==/UserScript==
 
 window.onload = function(){
 
-    GM_addStyle('.g-bd4 {background:#000 !important}')
+    GM_addStyle('.g-bd4 {background-color:#000 !important}')
     GM_addStyle('.m-table th, .m-table th .wp, .m-table td, .m-table .ply, .m-table .mv, .m-table .icn, .m-info .edit {background-color:#000 !important}')
-    GM_addStyle('.u-btn2, .u-btn2 i, .u-btn2 .icn, .u-btni, .u-btni i, .u-tag, .u-tag i, .u-btni-addply .ply {background:#000 !important}')
+    GM_addStyle('.u-btn2, .u-btn2 i, .u-btn2 .icn, .u-btni, .u-btni i, .u-tag, .u-tag i, .u-btni-addply .ply {background-color:#000 !important}')
       
     
     // GM_addStyle('.icn-add,.icn-fav,.icn-share,.icn-dl {padding: 1px; background-color:#ff0303 !important;}')
