eb62c42db61bed183ea3969ee005fd4c7cfab4ad
ryan <ryan.lq@hotmail.com>
2024-03-16 19:43:18 +0800

    update

--- violentmonkey_scripts/netmusic.js
+++ violentmonkey_scripts/netmusic.js
@@ -4,7 +4,7 @@
 // @match       https://music.163.com/*
 // @grant       GM_addStyle
 // @grant       unsafeWindow
-// @version     1.1
+// @version     1.2
 // @author      xdaoo
 // @description 2024/3/2 19:36:19
 // ==/UserScript==
